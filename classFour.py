8d413068524552f99c9c828945d2a071be4c5c1b
Cdmsahdioisau <chriszixulou2020@gmail.com>
2026-09-05 19:20:05 -0400

    Add files via upload

---
+++ classFour.py
@@ -0,0 +1,9 @@
+weatherlist = [
+    ["Day 1", ["Cloudy", 23, 15]],
+    ["Day 2", ["Cloudy", 24, 15]],
+    ["Day 3", ["Sunny", 23, 13]],
+    ["Day 4", ["Rainy", 23, 18]],
+    ["Day 5", ["Rainy", 26, 14]],
+    ["Day 6", ["Sunny", 21, 10]],
+    ["Day 7", ["Rainy", 21, None]]
+]
